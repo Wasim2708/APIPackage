09e4b2da2544a8c2a665f053edf5e3748da900d0
Wasim2708 <101546593+Wasim2708@users.noreply.github.com>
2024-03-27 09:49:16 +0530

    changed target name

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "APIPackage",
-            targets: ["APIPackage"]),
+            targets: ["ZohoDeskPortalAPIKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "APIPackage",
+        .binaryTarget(name: "ZohoDeskPortalAPIKit",
                       url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/3.0.0-beta-6/ZohoDeskPortalAPIKit.zip",
                       checksum: "ac624afb708a73447ffe9c8b3373655e9c17f0d2ac89f1080c1c0c4e52c01b5f")
     ]
